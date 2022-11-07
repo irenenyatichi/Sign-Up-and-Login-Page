217ca3ac4571dea4849656e894b76ec905df63a3
irenenyatichi <irenenyatichik@gmail.com>
2022-11-07 17:52:07 +0300

    Creating a user class with both the signUp and logIn methods

--- api/objects/user.php
+++ api/objects/user.php
@@ -1,3 +1,41 @@
 Contains properties and methods for “user” database queries
 
 <?php
+class User
+{
+
+    // database connection and table name
+    private $conn;
+    private $table_name = 'users';
+
+    // object properties
+    public $id;
+    public $username;
+    public $password;
+    public $created;
+
+    // constructor with $db as database connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // signup user
+    function signup()
+    {
+
+    }
+
+    // login user
+    function login()
+    {
+
+    }
+
+    // a function to check if username already exists
+    function isAlreadyExist()
+    {
+
+    }
+}
+?>
