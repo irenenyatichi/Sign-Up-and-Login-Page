cc63cc9e233095992f4bd43259763b55eebdc75d
irenenyatichi <irenenyatichik@gmail.com>
2022-11-09 12:34:51 +0300

    added and renamed files

--- api/user/login.php
+++ api/user/login.php
@@ -1,2 +1,18 @@
 file that will accept username & password and validate
 <?php
+function login(){
+    // select all query
+    $query = 'SELECT
+                `id`, `username`, `password`, `created`
+            FROM
+                ' . $this->table_name . " 
+            WHERE
+                username='".$this->username."' AND password='".$this->password."'";
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    // execute query
+    $stmt->execute();
+    return $stmt;
+}
