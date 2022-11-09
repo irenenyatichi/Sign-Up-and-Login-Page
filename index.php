4706d27649b72a4fd0394a490c28bf9f0c246a80
irenenyatichi <irenenyatichik@gmail.com>
2022-11-09 17:41:57 +0300

    Login and Signup Pages designed

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<?php
+session_start();
+    $_SESSION;
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <meta charset='UTF-8'>
+    <title>Account</title>
+    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
 </head>
 <body>
-
+    <a href="logout.php"> Logout</a>
+    <h1>This is the homepage</h1>
+    <br>
+    <h3>Hello, </h3>
 </body>
 </html>
